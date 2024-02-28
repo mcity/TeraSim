af2414c4e1f206ea5ec52332f1092b36cb9df897
Haowei Sun <printfplus@outlook.com>
2024-02-27 21:28:59 -0500

    remove carla api definition

--- terasim/overlay.py
+++ terasim/overlay.py
@@ -18,23 +18,3 @@ if not has_libsumo:
     except ImportError:
         logging.error("Unable to find traci!")
         raise
-
-# ===== load carla API =====
-has_carla = False
-
-# add Carla distro path
-if 'CARLA_ROOT' in os.environ:
-    api_path = pathlib.Path(os.environ['CARLA_ROOT']) / 'PythonAPI'
-    egg_name = 'carla-*%d.%d-%s.egg' % (
-        sys.version_info.major,
-        sys.version_info.minor,
-        'win-amd64' if os.name == 'nt' else 'linux-x86_64'
-    )
-    for egg in api_path.glob('./carla/dist/' + egg_name):
-        sys.path.append(egg)
-        break
-
-try:
-    import carla
-except ImportError:
-    logging.warning("Carla PythonAPI is not correctly installed!")
